7683bcd538159d1b36d97c58ff264837991eca75
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2019-09-14 17:23:27 +0100

    start

---
+++ src/MySelfLogLinker/LinkerSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace MySelfLogLinker
+{
+    public class Origin
+    {
+        public string ConnectionString { get; set; }
+        public string User { get; set; }
+        public string Pass { get; set; }
+    }
+    public class Destination
+    {
+        public string ConnectionString { get; set; }
+        public string User { get; set; }
+        public string Pass { get; set; }
+    }
+}
