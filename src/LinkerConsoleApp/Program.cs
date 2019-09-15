ac72da6e6053001da388d51aade2170eb6bfbc16
Riccardo Di Nuzzo <riccardo@dinuzzo.it>
2019-09-15 19:03:00 +0100

    implemented loading, configuring, starting multiple links with filters reading from appsettings.env.json; use ASPNETCORE_ENVIRONMENT to load your specific setting file (and set in your .gitignore)

--- src/LinkerConsoleApp/Program.cs
+++ src/LinkerConsoleApp/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using EventStore.ClientAPI;
 using EventStore.ClientAPI.SystemData;
 using Linker;
@@ -11,37 +13,51 @@ namespace LinkerConsoleApp
 {
     class Program
     {
-        private static Logger Log = LogManager.GetCurrentClassLogger();
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
         static void Main(string[] args)
         {
             Log.Info("Building services...");
+            var config = BuildConfig();
+            var links = config.GetSection("links").Get<IEnumerable<Link>>();
+            var services = new List<LinkerService>();
+            foreach (var link in links)
+            {
+                var filters = link.Filters.Select(linkFilter => new Filter
+                {
+                    FilterOperation = linkFilter.FilterOperation, FilterType = linkFilter.FilterType,
+                    Value = linkFilter.Value
+                }).ToList();
+                var filterService = new FilterService(filters);
+                var service = new LinkerService(new LinkerConnectionBuilder(new Uri(link.Origin.ConnectionString),
+                    ConnectionSettings.Create().SetDefaultUserCredentials(new UserCredentials(link.Origin.User, link.Origin.Pass)),
+                    link.Origin.ConnectionName), new LinkerConnectionBuilder(new Uri(link.Destination.ConnectionString),
+                    ConnectionSettings.Create().SetDefaultUserCredentials(new UserCredentials(link.Destination.User, link.Destination.Pass)),
+                    link.Destination.ConnectionName), filterService, Settings.Default(), new NLogger());
+                services.Add(service);
+            }
+            StartServices(services);
+            Log.Info("Press enter to exit the program");
+            Console.ReadLine();
+        }
 
-            var env = Environment.GetEnvironmentVariable("CORE_ENVIRONMENT");
+        private static async Task StartServices(IEnumerable<LinkerService> services)
+        {
+            foreach (var linkerService in services)
+            {
+                Log.Info($"Starting {linkerService.Name}");
+                await linkerService.Start();
+            }
+        }
+
+        private static IConfigurationRoot BuildConfig()
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
                 .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: false)
                 .AddEnvironmentVariables();
-            var config = builder.Build();
-
-            var origin = config.GetSection("origin").Get<Origin>();
-            var destination = config.GetSection("destination").Get<Destination>();
-            var filterService = new FilterService(new List<Filter>
-            {
-                new Filter { FilterOperation = FilterOperation.Exclude, FilterType = FilterType.Stream, Value = "diary-*" },
-                new Filter { FilterOperation = FilterOperation.Include, FilterType = FilterType.Stream, Value = "*" }
-            });
-
-            var service = new LinkerService(new LinkerConnectionBuilder(new Uri(origin.ConnectionString),
-                    ConnectionSettings.Create().SetDefaultUserCredentials(new UserCredentials(origin.User, origin.Pass)),
-                    origin.ConnectionName), new LinkerConnectionBuilder(new Uri(destination.ConnectionString),
-                    ConnectionSettings.Create().SetDefaultUserCredentials(new UserCredentials(destination.User, destination.Pass)),
-                    destination.ConnectionName), filterService, Settings.Default(), new NLogger());
-            service.Start().Wait();
-
-            Log.Info("Replica Service started");
-            Log.Info("Press enter to exit the program");
-            Console.ReadLine();
+            return builder.Build();
         }
     }
 }
